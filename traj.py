3e777e2c15fc46eb42383c621e415e97fb5d7783
wuhz <wuhz1020@gmail.com>
2025-10-01 22:16:07 +0800

    update code

--- traj.py
+++ traj.py
@@ -153,8 +153,8 @@ def main(args):
                                                 elif msg_tool_call['function']['name'] == "filesystem-write_file":
                                                     arg_s = json.loads(msg_tool_call['function']['arguments'])
                                                     dst.write(f"<div className=\"tool-call-box\">\n")
-                                                    # dst.write(f"{icon_map['filesystem']} `{msg_tool_call['function']['name']}`\n\n")
-                                                    dst.write(f"🛠 `{msg_tool_call['function']['name']}`\n\n")
+                                                    dst.write(f"{icon_map['filesystem']} `{msg_tool_call['function']['name']}`\n\n")
+                                                    # dst.write(f"🛠 `{msg_tool_call['function']['name']}`\n\n")
                                                     dst.write(f"```text workspace/{arg_s['path'].split('/')[-1].replace("@", "@")}\n")
                                                     dst.write(f"{arg_s['content'].replace("@", "@")}\n")
                                                     dst.write(f"```\n")
