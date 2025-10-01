008be68643afa1a62c96b3b5e45c064947c4e80b
wuhz <wuhz1020@gmail.com>
2025-10-02 01:55:55 +0800

    update code

--- traj.py
+++ traj.py
@@ -126,7 +126,7 @@ def main(args):
                                     if "tool_calls" in msg:
                                         if not (msg["content"] == "" or msg["content"] is None or msg["content"] == "null"):
                                             dst.write(f"<div className=\"thinking-box\">\n")
-                                            dst.write(f"🧐`Agent`\n\n{msg['content'].strip().replace("$", "")}\n</div>\n\n")
+                                            dst.write(f"🧐`Agent`\n\n{msg['content'].strip().replace("{", r"\{").replace("}", r"\}")}\n</div>\n\n")
 
                                         for msg_tool_call in msg["tool_calls"]:
                                             if msg_tool_call['type'] == "function":
@@ -240,7 +240,7 @@ def main(args):
                                         # tool_res = tool_res.replace('<', r'\<')
                                         # tool_res = tool_res.replace('>', r'\>')
                                         dst.write(f"<div className=\"result-box\">\n")
-                                        dst.write(f"🔍`tool result`\n```json\n{tool_res.replace("$", "")}\n```\n</div>\n\n")
+                                        dst.write(f"🔍`tool result`\n```json\n{tool_res}\n```\n</div>\n\n")
                                     else:
                                         raise NotImplementedError("tool result doesn't have content")
                                         # dst.write(f"<div className=\"result-box\">\n")
